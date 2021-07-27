f6b6237498a68f507376d559eac94b16559cfd99
birdue <rickoleyt@gmail.com>
2021-07-27 16:38:55 +0800

    Implemented textarea and rot13 logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button";
+import "./components/Button.css";
 
-function App() {
+
+
+const rot13 = (str) => {
+  let strRot13 = "";
+  str.split("").forEach(c => {
+    let ascii = c.charCodeAt(0);
+    if(65 <= ascii && ascii <= 90)
+      strRot13 += String.fromCharCode(65 + ((ascii - 65) + 13) % 26);
+    else if(97 <= ascii && ascii <= 122)
+      strRot13 += String.fromCharCode(97 + ((ascii - 97) + 13) % 26);
+    else
+      strRot13 += c;
+  });
+  return strRot13;
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Rot13!</h1>
+      <textarea name="" id="rot13-text" cols="30" rows="10"></textarea>
+      <Button text="Rot13" onClick={() => {
+        let rot13area = document.querySelector("#rot13-text");
+        rot13area.value = rot13(rot13area.value);
+      }}></Button>
     </div>
   );
 }
